4cfff21a0f744b7ad9568733d05c869bc1bfa898
Matteo Miraz <matteo.miraz@gmail.com>
2010-06-09 17:47:25 +0200

    Better JimpleWriter

--- testful/src/testful/coverage/soot/JimpleWriter.java
+++ testful/src/testful/coverage/soot/JimpleWriter.java
@@ -1,17 +1,17 @@
 /*
  * TestFul - http://code.google.com/p/testful/
  * Copyright (C) 2010  Matteo Miraz
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,14 +27,20 @@ import soot.SootField;
 import soot.SootMethod;
 import soot.Trap;
 import soot.Unit;
+import soot.jimple.AssignStmt;
 import soot.jimple.GotoStmt;
 import soot.jimple.IfStmt;
+import soot.jimple.InstanceInvokeExpr;
+import soot.jimple.InvokeExpr;
+import soot.jimple.InvokeStmt;
 import soot.jimple.LookupSwitchStmt;
+import soot.jimple.StaticInvokeExpr;
 import soot.jimple.TableSwitchStmt;
 import soot.tagkit.StringTag;
 
 /**
  * Utility class: write the jimple code to the screen
+ * @author matteo
  */
 public class JimpleWriter extends BodyTransformer {
 
@@ -75,22 +81,39 @@ public class JimpleWriter extends BodyTransformer {
 
 			System.out.printf("%4d ", i);
 
-			if(units[i] instanceof GotoStmt) System.out.println("goto " + findIndex(units, ((GotoStmt) units[i]).getTarget()));
-			else if(units[i] instanceof IfStmt) System.out.println("if " + ((IfStmt) units[i]).getCondition() + " goto " + findIndex(units, ((IfStmt) units[i]).getTarget()));
-			else if(units[i] instanceof TableSwitchStmt) {
+			if(units[i] instanceof GotoStmt) {
+				System.out.println("goto " + findIndex(units, ((GotoStmt) units[i]).getTarget()));
+
+			} else if(units[i] instanceof IfStmt) {
+				System.out.println("if " + ((IfStmt) units[i]).getCondition() + " goto " + findIndex(units, ((IfStmt) units[i]).getTarget()));
+
+			} else if(units[i] instanceof TableSwitchStmt) {
 				TableSwitchStmt sw = (TableSwitchStmt) units[i];
 				System.out.println("tswitch(" + sw.getKey() + ")");
 				final int lowIndex = sw.getLowIndex();
 				for(int t = 0; t <= sw.getHighIndex() - lowIndex; t++)
 					System.out.println("      case " + (t + lowIndex) + ": goto " + findIndex(units, sw.getTarget(t)));
 				System.out.println("      default: goto " + findIndex(units, sw.getDefaultTarget()));
+
 			} else if(units[i] instanceof LookupSwitchStmt) {
 				LookupSwitchStmt sw = (LookupSwitchStmt) units[i];
 				System.out.println("lswitch(" + sw.getKey() + ")");
 				for(int v = 0; v < sw.getTargetCount(); v++)
 					System.out.println("      case " + sw.getLookupValue(v) + ": goto " + findIndex(units, sw.getTarget(v)));
 				System.out.println("      default: goto " + findIndex(units, sw.getDefaultTarget()));
-			} else System.out.println(units[i].toString()); // + " " + units[i].getClass().getCanonicalName());
+
+			} else if(units[i] instanceof InvokeStmt) {
+				System.out.println(toString(((InvokeStmt) units[i]).getInvokeExpr()));
+
+			} else if(units[i] instanceof AssignStmt) {
+				if(((AssignStmt)units[i]).getRightOp() instanceof InvokeExpr)
+					System.out.println(((AssignStmt)units[i]).getLeftOp() + " = " + toString((InvokeExpr) ((AssignStmt)units[i]).getRightOp()));
+				else
+					System.out.println(units[i]);
+
+			} else {
+				System.out.println(units[i]);
+			}
 
 			if(units[i].hasTag("StringTag")) {
 				String info = ((StringTag) units[i].getTag("StringTag")).getInfo();
@@ -105,6 +128,31 @@ public class JimpleWriter extends BodyTransformer {
 			System.out.println(" " + findIndex(units, t.getBeginUnit()) + "-" + findIndex(units, t.getEndUnit()) + " -> (" + t.getException() + ") -> " + findIndex(units, t.getHandlerUnit()));
 
 		System.out.println();
+	}
+
+	private String toString(InvokeExpr i) {
+		StringBuilder sb = new StringBuilder();
+
+		if(i instanceof StaticInvokeExpr) {
+			sb.append(((StaticInvokeExpr)i).getMethod().getDeclaringClass().getName());
+
+		} else if(i instanceof InstanceInvokeExpr) {
+			sb.append(((InstanceInvokeExpr)i).getBase().toString());
+
+		} else
+			return i.toString();
+
+
+		sb.append(".").append(i.getMethod().getName());
+
+		sb.append("(");
+		for (int j = 0; j < i.getArgCount(); j++) {
+			if(j > 0) sb.append(", ");
+			sb.append(i.getArg(j).toString());
+		}
+		sb.append(")");
+
+		return sb.toString();
 	}
 
 	private int findIndex(Unit[] units, Unit target) {
